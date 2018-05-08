a7f274da0088924ce7ac0d246b1e3e5c1c745c09
Tiago Maluta <tmaluta@mgovbrasil.com.br>
2018-05-08 11:09:31 -0300

    Registra os models na interface do Admin
    Signed-off-by: Tiago Maluta <tmaluta@mgovbrasil.com.br>

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from app.models import *
+
+admin.site.register(SequenceGroups)
+admin.site.register(SequenceTheme)
+admin.site.register(SequenceSubTheme)
+admin.site.register(SequenceReview)
+admin.site.register(Client)
+admin.site.register(Project)
+admin.site.register(ProjectReview)
+admin.site.register(Message)
+admin.site.register(Sequence)
+admin.site.register(ProjectSequences)
