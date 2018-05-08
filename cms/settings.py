7f51ea10eaaf2911cc616ca759468569312ca345
Tiago Maluta <tmaluta@mgovbrasil.com.br>
2018-05-08 11:10:20 -0300

    - adiciona apps (apps e taggit) - aponta para o modelo de autenticação do usuário

--- cms/settings.py
+++ cms/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'taggit',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -99,7 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+AUTH_USER_MODEL = 'app.UserProfile'
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
