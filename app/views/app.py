3cd0faa3c4571f05ff411b08b10b8e3fb697db8f
Tiago Maluta <tmaluta@mgovbrasil.com.br>
2018-05-20 15:01:51 -0300

    Arquivo base das URLs
    Signed-off-by: Tiago Maluta <tmaluta@mgovbrasil.com.br>

---
+++ app/views/app.py
@@ -0,0 +1,15 @@
+from django.shortcuts import redirect, render
+from django.views.generic import TemplateView
+
+
+class SignUpView(TemplateView):
+    template_name = 'registration/signup.html'
+
+
+def home(request):
+    if request.user.is_authenticated:
+        if request.user.is_creator:
+            return redirect('creators:project_list')
+        else:
+            return redirect('reviewers:projectreview_list')
+    return render(request, 'app/home.html')
