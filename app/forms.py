a285c1e647eb3b6350a713471a2e003f80533d83
Tiago Maluta <tmaluta@mgovbrasil.com.br>
2018-05-20 12:36:07 -0300

    Formulários (primeiros)
    Signed-off-by: Tiago Maluta <tmaluta@mgovbrasil.com.br>

---
+++ app/forms.py
@@ -0,0 +1,48 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.db import transaction
+from django.forms.utils import ValidationError
+
+from lm.models import ProjectReview, ProjectSequences, Project
+
+
+class CreatorSignUpForm(UserCreationForm):
+    pass
+
+
+class SequenceForm(forms.ModelForm):
+    pass
+
+
+class ReviewerSignUpForm(UserCreationForm):
+    pass
+
+
+class BaseAnswerInlineFormSet(forms.BaseInlineFormSet):
+    pass
+
+
+class ProjectReviewAddForm(forms.ModelForm):
+    class Meta:
+        model = ProjectReview
+        fields = ('project_reviews', 'is_fact_flagged', 'is_activity_flagged', 'is_interaction_flagged',
+                  'is_reinforcement_flagged')
+
+class ProjectConfirmForm(forms.ModelForm):
+    is_project_approved = forms.BooleanField(required=True)
+    class Meta:
+        model = Project
+        fields = ('is_project_approved', )
+
+class ProjectSequencesReviewForm(forms.ModelForm):
+    pass
+
+
+class AddProject(forms.ModelForm):
+    client = forms.SelectMultiple()
+    project_name = forms.CharField(widget=forms.TextInput)
+    library = forms.SelectMultiple()
+
+    class Meta:
+        model = Project
+        fields = ('client', 'project_name', 'library', )
